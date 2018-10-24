ab2549d2c6dfd19f6d745abfe9cac1b551a4c28c
kebernet <rcooper@thoughtworks.com>
2018-10-24 09:30:09 -0400

    [Robert] Fixing filenames

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl apply -f etc/dev/load-balancer.yml
-kubectl apply -f etc/dev/pod.yml
+kubectl apply -f ./etc/dev/load-balancer.yaml
+kubectl apply -f ./etc/dev/pod.yaml
