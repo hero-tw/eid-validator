4e79ab4d5cd85eb9afd17314bb157de01d1f984f
kebernet <rcooper@thoughtworks.com>
2018-10-24 10:31:33 -0400

    [Robert] Still trying to get the deploy to work.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 aws-iam-authenticator init -i $CLUSTER_NAME
+kubectl --kubeconfig heptio-authenticator-aws.kubeconfig
 kubectl apply -f ./etc/dev/load-balancer.yaml
 kubectl apply -f ./etc/dev/pod.yaml
